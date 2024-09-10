a38c0ba3c615d632ca6abc7fb4547b3cbd752d7d
luanfsouza <luanferreira7777@gmail.com>
2024-09-10 14:16:29 -0300

    carinho de compras shopee

--- node-modules/shopee-cart/src/index.js
+++ node-modules/shopee-cart/src/index.js
@@ -1,10 +1,27 @@
+import * as cartService from "./services/cart.js";
+
 import createItem from "./services/item.js";
 
 const cart = [];
+const myWhishList = [];
 
 console.log("Hello shopee");
 
-const item1 = await createItem("hotwheels ferrari", 20.99, 1);
+const item1 = await createItem("hotwheels ferrari", 20.99, 6);
 const item2 = await createItem("hotwheels lamborghini", 39.99, 3);
 
+await cartService.addItem(cart, item1);
+await cartService.addItem(cart, item2);
+await cartService.addItem(myWhishList, item2);
+
+await cartService.calculateTotal(cart);
 console.log(item2.subTotal());
+
+await cartService.deleteItem(cart, item1.name);
+await cartService.deleteItem(cart, item1.name);
+await cartService.deleteItem(cart, item2.name);
+await cartService.displayCart(cart);
+
+await cartService.removeItem(cart, item1.name);
+await cartService.removeItem(cart, item2.name);
+await cartService.displayCart(cart);
--- node-modules/shopee-cart/src/services/cart.js
+++ node-modules/shopee-cart/src/services/cart.js
@@ -1,15 +1,49 @@
-async function addItem(userCart){
-    
+async function addItem(userCart, item) {
+  userCart.push(item);
 }
 
-async function deleteItem(userCart, name){
-    
+async function deleteItem(userCart, name) {
+  for (let i = 0; i < userCart.length; i++) {
+    if (userCart[i].name == name) {
+      if (userCart[i].quantity > 1) {
+        userCart[i].quantity--;
+
+        break;
+      } else {
+        userCart.splice(i, 1);
+        break; 
+      }
+    }
+  }
 }
 
-async function removeItem(userCart, index){
-    
+async function displayCart(userCart) {
+  console.log("Shopee cart list:");
+  userCart.forEach((item, index) => {
+    console.log(
+      `${index + 1}. ${item.name} - R$ ${item.price} | ${
+        item.quantity
+      } | ${item.subTotal()}`
+    );
+  });
 }
 
-async function calculateTotal(userCart){
-    
+async function removeItem(userCart, name) {
+  const index = userCart.findIndex((item) => item.name === name);
+  if (index != -1) {
+    userCart.splice(index, 1);
+  }
+  // userCart.splice(index, 1);
 }
+
+async function calculateTotal(userCart) {
+  console.log(
+    "total: " +
+      userCart.reduce(
+        (acumulator, itemAtual) => acumulator + itemAtual.subTotal(),
+        0
+      )
+  );
+}
+
+export { addItem, calculateTotal, deleteItem, removeItem, displayCart };
