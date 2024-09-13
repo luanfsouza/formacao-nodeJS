7e53ed7b3e834d9db142f3d79300ddd2d859df84
luanfsouza <luanferreira7777@gmail.com>
2024-09-12 23:20:27 -0300

    .

---
+++ gerenciando-pacotes-node-npm/apagar/src/index.js
@@ -0,0 +1,12 @@
+import { Agents, ValorantApiCom, Weapons } from "@valapi/valorant-api.com";
+
+import logsymbols from "log-symbols"
+
+console.log(logsymbols.success, "tudo certo")
+// const client = new ValorantApiCom({
+//   language: "en-US",
+// });
+// const weapons = client.Weapons.getSkins().then((skins) => {
+//   console.log();
+//   skins.data.data.forEach((item)=>console.log(item.displayIcon))
+// });
